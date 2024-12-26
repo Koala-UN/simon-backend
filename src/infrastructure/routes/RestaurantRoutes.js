3e12b03b27ab6a76e483eef2793d0c21ad0848b9
brosgor <alfonso.suarez.dev@gmail.com>
2024-12-26 18:20:03 -0500

    modified: renombrar rutas

--- src/infrastructure/routes/RestaurantRoutes.js
+++ src/infrastructure/routes/RestaurantRoutes.js
@@ -6,8 +6,8 @@ const router = express.Router();
 // Rutas de restaurantes
 router.post('/', restaurantController.createRestaurant);
 router.get('/:restaurantId', restaurantController.getRestaurantById);
-router.get('/ciudad/:cityId', restaurantController.getAllRestaurantsByCity);
-router.get('/departamento/:departmentId', restaurantController.getAllRestaurantsByDepartment);
-router.get('/pais/:countryId', restaurantController.getAllRestaurantsByCountry);
+router.get('/city/:cityId', restaurantController.getAllRestaurantsByCity);
+router.get('/department/:departmentId', restaurantController.getAllRestaurantsByDepartment);
+router.get('/country/:countryId', restaurantController.getAllRestaurantsByCountry);
 
 module.exports = router;
