1847f9c4271bc2d4e31e6aa50414a2cb8e28c769
brosgor <alfonso.suarez.dev@gmail.com>
2024-12-25 21:56:28 -0500

    modified

--- src/database/database.js
+++
@@ -1,7 +0,0 @@
-// MySQL connection setup
-const mysql = require('mysql2/promise');
-const config = require('../config/config');
-
-const pool = mysql.createPool(config.db);
-
-module.exports = pool;
