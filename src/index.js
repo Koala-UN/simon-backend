7b15d57433c9ea5e18c8d1f070eda320309c1868
CesarFRR <crinconro@unal.edu.co>
2025-02-21 17:30:02 -0500

    fibug: port 5

--- src/index.js
+++ src/index.js
@@ -1,15 +1,8 @@
 const app = require('./app');
-const config = require('./config/config');
-const cloudinary = require('cloudinary').v2;
+// const config = require('./config/config');
+// const cloudinary = require('cloudinary').v2;
 
 const port = process.env.PORT || 5000;
-app.listen(8080, () => {
-    console.log(`Server running at http://localhost:${8080}`);
-});
-
-// Configura Cloudinary
-cloudinary.config({
-  cloud_name: config.cloudinary.cloud_name,
-  api_key: config.cloudinary.api_key,
-  api_secret: config.cloudinary.api_secret,
+app.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}`);
 });
