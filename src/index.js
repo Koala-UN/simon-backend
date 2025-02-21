719fa88c2c019ab0a41c557998ac11a0b36e9553
CesarFRR <crinconro@unal.edu.co>
2025-02-21 16:51:51 -0500

    fixbug: port 3

--- src/index.js
+++ src/index.js
@@ -2,8 +2,9 @@ const app = require('./app');
 const config = require('./config/config');
 const cloudinary = require('cloudinary').v2;
 
-app.listen(config.app.port, () => {
-    console.log(`Server running at http://localhost:${config.app.port}`);
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}`);
 });
 
 // Configura Cloudinary
