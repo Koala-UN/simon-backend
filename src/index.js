c3e343ab3001bd8539ebaa7141f3f8892527ba9d
CesarFRR <crinconro@unal.edu.co>
2025-02-21 17:14:18 -0500

    fixbug: port 8080

--- src/index.js
+++ src/index.js
@@ -3,8 +3,8 @@ const config = require('./config/config');
 const cloudinary = require('cloudinary').v2;
 
 const port = process.env.PORT || 5000;
-app.listen(port, () => {
-    console.log(`Server running at http://localhost:${port}`);
+app.listen(8080, () => {
+    console.log(`Server running at http://localhost:${8080}`);
 });
 
 // Configura Cloudinary
