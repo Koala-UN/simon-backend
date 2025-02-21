5851bc881d1bd283055fa233313113eb5a87dea6
CesarFRR <crinconro@unal.edu.co>
2025-02-21 18:42:16 -0500

    fixbug: cloudinary credentials

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
 const app = require('./app');
 const config = require('./config/config');
-// const cloudinary = require('cloudinary').v2;
+const cloudinary = require('cloudinary').v2;
 
 app.listen(config.app.port, () => {
     console.log(`Server running at http://localhost:${config.app.port}`);
 });
 
-
-// test
+// Configura Cloudinary
+cloudinary.config({
+  cloud_name: config.cloudinary.cloud_name,
+  api_key: config.cloudinary.api_key,
+  api_secret: config.cloudinary.api_secret,
+});
