d6f6984f2eba66a04b55a79c05ab11762f4095b6
CesarFRR <crinconro@unal.edu.co>
2025-02-21 18:09:30 -0500

    fixbug: env config

--- src/config/config.js
+++ src/config/config.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 
 module.exports = {
     app: {
-        port: process.env.PORT || 5000,
+        port: process.env.PORT || 4000,
         frontendURL: process.env.FRONTEND_URL || 'http://localhost:5173',
         backendURL: process.env.BACKEND_URL || 'http://localhost:5000',
     },
